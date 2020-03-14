4f6c109b8a6e07f855ae01b201d3147176345317
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-14 18:46:37 +0530

    added Web3.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+  
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
   render() {
     return (
       <div>
