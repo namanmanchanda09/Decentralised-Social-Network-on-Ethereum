9e671d38531d67d4abea51ad041d2ce36c5e229f
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-14 11:41:49 +0530

    created CreatePost function

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,6 +2,8 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract SocialNetwork{
     string public name;
+    uint public postCount = 0;
+    mapping(uint => Post) public posts;
     struct Post{
         uint id;
         string content;
@@ -11,9 +13,9 @@ contract SocialNetwork{
     constructor() public{
         name = "Dapp University Social Network";
     }
-
-    function createPost(string memory _content) public pure{
-        _content;
+    function createPost(string memory _content)public {
+        postCount ++;
+        posts[postCount] = Post(postCount,_content,0,msg.sender);
     }
 }
 
