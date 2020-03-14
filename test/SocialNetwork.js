eb2de3fe6780e5fc7f01b883ee814fcf6db3ba48
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-14 11:07:29 +0530

    test for deployment and name of network

--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -5,8 +5,25 @@ require('chai')
     .should()
 
 contract('SocialNetwork',(accounts)=>{
-    
+    let socialNetwork
+    before(async ()=>{
+        socialNetwork = await SocialNetwork.deployed()
+    })
 
+    describe('deployment',async ()=>{
+        it('deploys successfully',async ()=>{
+            const address = await SocialNetwork.address
+            assert.notEqual(address,0x0)
+            assert.notEqual(address,'')
+            assert.notEqual(address,null)
+            assert.notEqual(address,undefined)
+        })
 
+        it('has a name',async ()=>{
+            const name = await socialNetwork.name()
+            assert.equal(name,'Dapp University Social Network')
+
+        })
+    })
 })
 
