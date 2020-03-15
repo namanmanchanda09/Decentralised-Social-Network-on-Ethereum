4b1cb3aadba804e3080b8dd6bf773dacdc3d86b1
namanmanchanda09 <naman.mohan09@outlook.com>
2020-03-15 12:42:47 +0530

    posts published on front-end

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import Navbar from './Navbar'
 import SocialNetwork from '../abis/SocialNetwork.json'
+import Identicon from 'identicon.js'
 
 
 
@@ -78,14 +79,27 @@ class App extends Component {
                     return(
                       <div className="card mb-4" key={key}>
                         <div className="card-header">
-                          <small className="text-muted">Post header</small>
+                          <img className="ml-2"
+                            width='30'
+                            height='30'
+                            src={`data:image/png;base64,${new Identicon(this.state.account, 30).toString()}`}
+                            alt="Avatar"
+                          />
+                          <small className="text-muted">{post.author}</small>
                         </div>
                         <ul id="postList" className="list-group list-group-flush">
                           <li className="list-group-item">
-                            <p>Post Body</p>
+                            <p>{post.content}</p>
                           </li>
                           <li key={key} className="list-group-item py-2">
-                            <p>Post Footer</p>
+                            <small className="float-left mt-1 text-muted">
+                              TIPS : {window.web3.utils.fromWei(post.tipAmount.toString(),'Ether')} ETH
+                            </small>
+                            <button className="btn btn-link btn-sm float-right pt-0">
+                              <span>
+                                TIP 0.1 ETH
+                              </span>
+                            </button>
                           </li>
                         </ul>
                       </div>
